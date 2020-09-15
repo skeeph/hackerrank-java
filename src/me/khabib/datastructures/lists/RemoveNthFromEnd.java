3e25b75cd406ac1f3b1c110340be679a926a33b9
Khabib Murtuzaaliev <murtuzaaliv@i-teco.ru>
2020-09-15 09:19:21 +0300

    19. Remove Nth Node From End of List

--- src/me/khabib/datastructures/lists/RemoveNthFromEnd.java
+++ src/me/khabib/datastructures/lists/RemoveNthFromEnd.java
@@ -1,4 +1,38 @@
 package me.khabib.datastructures.lists;
 
+/**
+ * https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+ */
 public class RemoveNthFromEnd {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null) return null;
+        ListNode fast = head;
+        ListNode slow = head;
+
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+
+        if (fast == null) {
+            return head.next;
+        }
+
+        fast = fast.next;
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(
+                new RemoveNthFromEnd().removeNthFromEnd(
+                        ListNode.build(1), 1
+                )
+        );
+    }
 }
