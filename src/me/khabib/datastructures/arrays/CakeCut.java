3e727776396228ad2e336e8c9a738f8a755ae141
Khabib Murtuzaaliev <khabib_murtuzaaliev@epam.com>
2021-06-03 10:38:16 +0300

    1465. Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts

--- src/me/khabib/datastructures/arrays/CakeCut.java
+++ src/me/khabib/datastructures/arrays/CakeCut.java
@@ -26,29 +26,6 @@ public class CakeCut {
         return hd;
     }
 
-
-//    public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
-//        long longestH = getMaxSize(h, horizontalCuts);
-//        long longestV = getMaxSize(w, verticalCuts);
-//        int mod = 1000000007;
-//        long ans = ((longestH%mod)*(longestV%mod))%mod;
-//        return (int)ans;
-//    }
-//
-//    private long getMaxSize(int max, int[] cuts){
-//        Arrays.sort(cuts);
-//        long init = 0;
-//        long res = 0;
-//        for(int i = 0;i<cuts.length;i++){
-//            long dist = cuts[i]-init;
-//            res = Math.max(res,dist);
-//            init = cuts[i];
-//        }
-//        res = Math.max(res,max-cuts[cuts.length-1]);
-//        return res;
-//    }
-
-
     public static void main(String[] args) {
         System.out.println(new CakeCut().maxArea(5, 4, new int[]{3, 1}, new int[]{1}));
     }
